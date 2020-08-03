4fc052b88598f1c5d7c7adee1516b90f22d79084
eharari <e.harari@yahoo.com>
2020-08-03 16:32:44 -0600

    hola

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express()
+const app = express();
+const cors = require('cors');
 const { config } = require('../config')
 const mongoose = require('mongoose')
 
